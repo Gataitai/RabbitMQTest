261cafbc51dbf6e9bcdaf3915c8ebecd5a2b71bb
gataitai <mpattipeilohy1@gmail.com>
2023-11-17 16:32:25 +0100

    changes

--- src/main/java/Consumer.java
+++ src/main/java/Consumer.java
@@ -1,59 +1,83 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.DeliverCallback;
+import com.rabbitmq.client.*;
 
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.concurrent.TimeoutException;
 import Message.*;
-
 public class Consumer {
     private static final String QUEUE_NAME = "example_queue";
+    private final ObjectMapper objectMapper;
+
+    public Consumer() {
+        this.objectMapper = new ObjectMapper();
+    }
 
     public static void main(String[] args) throws IOException, TimeoutException {
+        Consumer consumer = new Consumer();
+        consumer.run();
+    }
+
+    private void run() throws IOException, TimeoutException {
         try (Connection connection = getConnection();
              Channel channel = connection.createChannel()) {
 
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            declareQueue(channel);
 
-            Thread terminationListener = new Thread(() -> {
-                System.out.println("Press 'q' to terminate the consumer.");
-                Scanner scanner = new Scanner(System.in);
-                while (true) {
-                    if (scanner.nextLine().equalsIgnoreCase("q")) {
-                        System.out.println("Terminating consumer...");
-                        System.exit(0);
-                    }
-                }
-            });
+            Thread terminationListener = createTerminationListener();
             terminationListener.start();
 
-            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-                ObjectMapper objectMapper = new ObjectMapper();
-                Message receivedMessage = objectMapper.readValue(delivery.getBody(), Message.class);
-                System.out.println(" [x] Received '" + receivedMessage.getText() + "'");
-
-                switch (receivedMessage.getMessageType()) {
-                    case MESSAGE:
-                        Message responseMessage = new Message(MessageType.OK, "hello back");
-
-                        byte[] responseBytes = objectMapper.writeValueAsBytes(responseMessage);
-
-                        channel.basicPublish("", delivery.getProperties().getReplyTo(), null, responseBytes);
-
-                        System.out.println(" [x] Sent response '" + responseMessage.getText() + "'");
-                        break;
-                    // Add more cases for other message types if needed
-                }
-            };
-
-            channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {});
+            DeliverCallback deliverCallback = createDeliverCallback(channel);
+            channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
+            });
         }
     }
 
-    private static Connection getConnection() throws IOException, TimeoutException {
+    private void declareQueue(Channel channel) throws IOException {
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+    }
+
+    private Thread createTerminationListener() {
+        return new Thread(() -> {
+            System.out.println("Press 'q' to terminate the consumer.");
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                if (scanner.nextLine().equalsIgnoreCase("q")) {
+                    System.out.println("Terminating consumer...");
+                    System.exit(0);
+                }
+            }
+        });
+    }
+
+    private DeliverCallback createDeliverCallback(Channel channel) {
+        return (consumerTag, delivery) -> {
+            Message receivedMessage = objectMapper.readValue(delivery.getBody(), Message.class);
+            System.out.println(" [x] Received '" + receivedMessage.getText() + "'");
+
+            handleMessageType(channel, delivery, receivedMessage);
+        };
+    }
+
+    private void handleMessageType(Channel channel, Delivery delivery, Message receivedMessage) throws IOException {
+        switch (receivedMessage.getMessageType()) {
+            case MESSAGE:
+                sendResponse(channel, delivery);
+                break;
+            // Add more cases for other message types if needed
+        }
+    }
+
+    private void sendResponse(Channel channel, Delivery delivery) throws IOException {
+        Message responseMessage = new Message(MessageType.OK, "hello back");
+        byte[] responseBytes = objectMapper.writeValueAsBytes(responseMessage);
+
+        channel.basicPublish("", delivery.getProperties().getReplyTo(), null, responseBytes);
+
+        System.out.println(" [x] Sent response '" + responseMessage.getText() + "'");
+    }
+
+    private Connection getConnection() throws IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         return factory.newConnection();
--- src/main/java/Producer.java
+++ src/main/java/Producer.java
@@ -1,60 +1,105 @@
+import Message.Message;
 import Message.MessageType;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.rabbitmq.client.AMQP;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.*;
 
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.UUID;
 import java.util.concurrent.TimeoutException;
-import Message.*;
-import com.rabbitmq.client.ConnectionFactory;
 
-class Producer {
+public class Producer {
     private static final String QUEUE_NAME = "example_queue";
     private static final String EXCHANGE_NAME = "";
     private static final String ROUTING_KEY = QUEUE_NAME;
 
+    private final ObjectMapper objectMapper;
+    private final String callbackQueue;
+
+    public Producer() {
+        this.objectMapper = new ObjectMapper();
+        this.callbackQueue = createCallbackQueue();
+    }
+
     public static void main(String[] args) throws IOException, TimeoutException {
+        Producer producer = new Producer();
+        producer.run();
+    }
+
+    private void run() throws IOException, TimeoutException {
         try (Connection connection = getConnection();
              Channel channel = connection.createChannel()) {
 
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            declareQueue(channel);
+            declareCallbackQueue(channel);
 
-            Thread terminationListener = new Thread(() -> {
-                System.out.println("Press 'q' to terminate the producer.");
-                Scanner scanner = new Scanner(System.in);
-                while (true) {
-                    if (scanner.nextLine().equalsIgnoreCase("q")) {
-                        System.out.println("Terminating producer...");
-                        System.exit(0);
-                    }
-                }
-            });
+            Thread terminationListener = createTerminationListener();
             terminationListener.start();
 
-            do {
-                Message message = new Message(MessageType.MESSAGE, "hello");
-
-                ObjectMapper objectMapper = new ObjectMapper();
-                byte[] messageBytes = objectMapper.writeValueAsBytes(message);
-
-                AMQP.BasicProperties properties = new AMQP.BasicProperties.Builder()
-                        .correlationId(UUID.randomUUID().toString())
-                        .replyTo(QUEUE_NAME)
-                        .build();
-
-                channel.basicPublish(EXCHANGE_NAME, ROUTING_KEY, properties, messageBytes);
-
-                System.out.println(" [x] Sent '" + message.getText() + "'");
-            } while (true);
+            // Send a message and wait for the response
+            sendMessage(channel);
         }
     }
 
-    private static Connection getConnection() throws IOException, TimeoutException {
+    private void declareQueue(Channel channel) throws IOException {
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+    }
+
+    private void declareCallbackQueue(Channel channel) throws IOException {
+        channel.queueDeclare(callbackQueue, false, false, true, null);
+    }
+
+    private Thread createTerminationListener() {
+        return new Thread(() -> {
+            System.out.println("Press 'q' to terminate the producer.");
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                if (scanner.nextLine().equalsIgnoreCase("q")) {
+                    System.out.println("Terminating producer...");
+                    System.exit(0);
+                }
+            }
+        });
+    }
+
+    private void sendMessage(Channel channel) throws IOException {
+        // Generate a unique correlation ID for this message
+        String correlationId = UUID.randomUUID().toString();
+
+        // Send the message
+        Message message = new Message(MessageType.MESSAGE, "hello");
+        byte[] messageBytes = objectMapper.writeValueAsBytes(message);
+
+        AMQP.BasicProperties properties = new AMQP.BasicProperties.Builder()
+                .correlationId(correlationId)
+                .replyTo(callbackQueue)
+                .build();
+
+        // Publish the message
+        channel.basicPublish(EXCHANGE_NAME, ROUTING_KEY, properties, messageBytes);
+
+        System.out.println(" [x] Sent '" + message.getText() + "' with Correlation ID: " + correlationId);
+
+        // Set up the consumer for the specific correlation ID after publishing
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            if (correlationId.equals(delivery.getProperties().getCorrelationId())) {
+                Message responseMessage = objectMapper.readValue(delivery.getBody(), Message.class);
+                System.out.println(" [x] Received response '" + responseMessage.getText() + "' for Correlation ID: " + correlationId);
+            }
+        };
+
+        channel.basicConsume(callbackQueue, true, deliverCallback, consumerTag -> {
+        });
+    }
+
+
+    private Connection getConnection() throws IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         return factory.newConnection();
+    }
+
+    private String createCallbackQueue() {
+        return "callback_queue_" + UUID.randomUUID().toString();
     }
 }
